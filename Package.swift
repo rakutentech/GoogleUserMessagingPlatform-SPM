d55f7c79eea5a61f9c546596f8d44d6837c1384a
Kazuma Koze <kazumakoze@gmail.com>
2021-02-20 14:34:49 +0900

    Separate target and binary target to add linker setting

--- Package.swift
+++ Package.swift
@@ -21,10 +21,18 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "UserMessagingPlatform",
+                dependencies: [
+                    .target(name: "UserMessagingPlatformBinary")
+                ],
+                path: "Sources/dummy",
+                linkerSettings: [
+                    .linkedFramework("WebKit")
+                ]),
         .binaryTarget(
-            name: "UserMessagingPlatform",
+            name: "UserMessagingPlatformBinary",
             path: "Sources/UserMessagingPlatform.xcframework"),
-//        .binaryTarget(name: "UserMessagingPlatform",
+//        .binaryTarget(name: "UserMessagingPlatformBinary",
 //                      url: "",
 //                      checksum: ""),
     ]
---
+++ Sources/dummy/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  dummy.swift
+//  
+//
+//  Created by Kazuma Koze on 2021/02/20.
+//
+
+import Foundation
