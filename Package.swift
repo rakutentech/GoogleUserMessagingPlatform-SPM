46c762be7396b4c15d9373d412b11d0ad1d1e229
Kazuma Koze <kazumakoze@gmail.com>
2021-04-29 09:22:18 +0900

    Update the reference of cocoapods spec

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// ref. https://github.com/CocoaPods/Specs/blob/00c12183a9d43a1665d74bff312058307c72a59d/Specs/7/e/f/GoogleUserMessagingPlatform/1.4.0/GoogleUserMessagingPlatform.podspec.json
+// ref. https://github.com/CocoaPods/Specs/blob/1f894cf1c5d71701f2ea32f85e5988b7775632dc/Specs/7/e/f/GoogleUserMessagingPlatform/2.0.0/GoogleUserMessagingPlatform.podspec.json
 
 import PackageDescription
 
