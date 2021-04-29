b7f2f97e160d957d6981993397ede9637c2588f3
Kazuma Koze <kazumakoze@gmail.com>
2021-04-29 09:36:14 +0900

    Update the reference of cocoapods spec

--- Sources/weak_import.swift
+++ Sources/weak_import.swift
@@ -5,7 +5,7 @@
 //  Created by Kazuma Koze on 2021/03/10.
 //
 
-// ref. https://github.com/CocoaPods/Specs/blob/00c12183a9d43a1665d74bff312058307c72a59d/Specs/7/e/f/GoogleUserMessagingPlatform/1.4.0/GoogleUserMessagingPlatform.podspec.json#L25-L27
+// ref. https://github.com/CocoaPods/Specs/blob/1f894cf1c5d71701f2ea32f85e5988b7775632dc/Specs/7/e/f/GoogleUserMessagingPlatform/2.0.0/GoogleUserMessagingPlatform.podspec.json#L29-L31
 
 #if canImport(AdSupport)
 import AdSupport
