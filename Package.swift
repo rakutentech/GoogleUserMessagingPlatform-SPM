fa5e39e6f0bcafbca2a940e359cb66295ea84409
Kazuma Koze <kazumakoze@gmail.com>
2021-11-02 13:42:42 +0900

    Update swift-tools-version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // ref. https://github.com/CocoaPods/Specs/blob/1f894cf1c5d71701f2ea32f85e5988b7775632dc/Specs/7/e/f/GoogleUserMessagingPlatform/2.0.0/GoogleUserMessagingPlatform.podspec.json
